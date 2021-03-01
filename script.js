9d340c2a59f274690993b843d067e8ed36c5d3c2
trysten_h <71472287+hesstry@users.noreply.github.com>
2021-02-28 19:40:30 -0800

    added post request

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var session = require('express-session');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+app.use(session({secret:'393790cae6e47c9adf022a8fb678becd3b3fb5109b80a4ab089dd90971d4a7a44ddb7911d780328600c6e1412925d7bd0a8b8915f49e1e8fde9d4185c336fa95'}));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -27,6 +28,34 @@ app.get('/contactme',function(req,res){
 
 app.get('/projects',function(req,res){
   res.render('projects');
+});
+
+app.post('/contactme', function(req, res){
+
+  let context = {'data':{}};
+  console.log(req.body);
+
+  if (req.body.name){
+    context.data.name = req.body.name;
+    context.data.email = req.body.email;
+    context.data.message = req.body.message;
+  }
+
+  console.log(context);
+
+  res.render('contactme', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
