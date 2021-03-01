93e8cd5bbb162a63e65039b70c4dfbf92e71f21f
trysten_h <71472287+hesstry@users.noreply.github.com>
2021-02-28 18:48:17 -0800

    changed port to match school servers

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ var session = require('express-session');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use(session({secret:'393790cae6e47c9adf022a8fb678becd3b3fb5109b80a4ab089dd90971d4a7a44ddb7911d780328600c6e1412925d7bd0a8b8915f49e1e8fde9d4185c336fa95'}));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 5345);
 
 app.get('/',function(req,res){
   res.render('homepage');
